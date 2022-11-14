38f6140c4f2a3f1820210954ea161db29b31be2c
30710 <3071089870@qq.com>
2022-11-14 23:23:04 +0800

    init commit

---
+++ tabbar/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+     <tab-bar>
+       <tab-bar-item path="/home" active-color="blue">
+         <img slot="item-icon" src="./assets/image/tabbar/home.png" alt="">
+         <img slot="item-icon-active" src="./assets/image/tabbar/home-active.png" alt="">
+         <div slot="item-text">首页</div>
+       </tab-bar-item>
+       <tab-bar-item path="/category" active-color="blue">
+         <img slot="item-icon" src="./assets/image/tabbar/home.png" alt="">
+         <img slot="item-icon-active" src="./assets/image/tabbar/home-active.png" alt="">
+         <div slot="item-text">首页2</div>
+       </tab-bar-item>
+     </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
+export default {
+  name: 'App',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+</style>
---
+++ tabbar/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  }, {
+    path: '/home',
+    component: Home
+  }, {
+    path: '/category',
+    component: Category
+  }
+]
+const router = new VueRouter({
+  routes
+})
+export default router
